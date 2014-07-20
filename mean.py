01b4a74c290ef281d7169017a73515acbdb658fe
Chris Boehm <chrisboe@umich.edu>
2014-07-20 12:16:09 -0400

    Final changes made

--- mean.py
+++ mean.py
@@ -6,29 +6,6 @@ sum = 0
 n = 0
 
 # Modifying Chris' program
-# bla
-
-# bla
-# bla
-# bla
-# bla
-# blu
-# bla
-# bla
-# flu
-# bla
-# bla
-# bla
-
-
-
-
-
-
-
-
-
-#glu
 
 # Sum input values
 for numb in open('data.txt'):
